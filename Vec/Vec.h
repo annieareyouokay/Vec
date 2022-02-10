f65c4483586d8b382e496eba38ec58f3cf625bc4
straponjimmy <telecaster068@gmail.com>
2022-02-10 16:52:16 +1000

    Добавьте файлы проекта.

---
+++ Vec/Vec.h
@@ -0,0 +1,11 @@
+#pragma once
+
+template<class T> class Vec
+{
+public:
+	//интерфейс класса
+private:
+	//реализация класса
+	T* data; //first element of array
+	T* limit //element next for the last element of array
+};
